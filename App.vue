148e03022655be38115a8ab3e88a90a4eaf64be4
Ismael Placa <sempressimo@hotmail.com>
2021-06-19 20:41:35 -0400

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,39 +1,68 @@
 <template>
-  <v-app>
-    <v-navigation-drawer app> </v-navigation-drawer>
-    <v-app-bar app> Github Candidates Search </v-app-bar>
-    <v-main>
-      <v-container fluid style="padding: 35px">
-        <SearchGitRepos />
-      </v-container>
-    </v-main>
+    <v-app>
+        <v-navigation-drawer permanent expand-on-hover app>
+        </v-navigation-drawer>
+        <v-app-bar app>
+            <v-container fluid>
+                <v-layout>
+                    <v-flex md1>
+                        <v-img
+                            max-height="32"
+                            max-width="32"
+                            src="@/assets/GitHub-Mark-32px.png"
+                        ></v-img>
+                    </v-flex>
+                    <v-flex md10>
+                        <span>Github Candidates Search </span>
+                    </v-flex>
+                    <v-flex md1>
+                        <div class="text-right">
+                            <v-switch
+                                label="Dark Mode"
+                                @change="toggleDarkTheme()"
+                            ></v-switch>
+                        </div>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-app-bar>
+        <v-main>
+            <v-container fluid style="padding: 35px">
+                <SearchGitRepos />
+            </v-container>
+        </v-main>
 
-    <v-footer app style="text-align: center">
-      <v-container fluid>
-        <v-layout>
-          <v-flex md2></v-flex>
-          <v-flex md8 style="font-size: small">
-            (2021) Ismael Placa, All Rights Reserved.
-          </v-flex>
-          <v-flex md2></v-flex>
-        </v-layout>
-      </v-container>
-    </v-footer>
-  </v-app>
+        <v-footer app style="text-align: center" dark padless>
+            <v-container fluid>
+                <v-layout>
+                    <v-flex md2></v-flex>
+                    <v-flex md8 style="font-size: small">
+                        (2021) Ismael Placa, All Rights Reserved.
+                    </v-flex>
+                    <v-flex md2></v-flex>
+                </v-layout>
+            </v-container>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
 import SearchGitRepos from "./components/SearchGitRepos.vue";
 
 export default {
-  name: "App",
-  components: {
-    SearchGitRepos,
-  },
-  data() {
-    return {
-      results: [],
-    };
-  },
+    name: "App",
+    components: {
+        SearchGitRepos,
+    },
+    methods: {
+        toggleDarkTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        },
+    },
+    data() {
+        return {
+            results: [],
+        };
+    },
 };
 </script>
